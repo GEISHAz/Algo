ec4e550b947b8494d25a4b64bca56aca64d289b7
Geishaz <allmin9702@naver.com>
2024-05-24 09:22:13 +0900

    [Algo] add: p멀리뛰기 #20240524

---
+++ Programmers/p멀리뛰기.java
@@ -0,0 +1,13 @@
+class p멀리뛰기 {
+    public long solution(int n) {
+        long dp[] = new long[2001];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        if(n < 3) return n;
+        for(int i = 3 ; i <= n ; i++ ){
+            dp[i] = dp[i-1] + dp[i-2] %1234567;
+        }
+        return dp[n];
+    }
+}
