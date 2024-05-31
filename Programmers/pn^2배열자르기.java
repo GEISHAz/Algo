503863782a895265aebd8f34fbbd5f09a9e23cea
GEISHAz <allmin9702@naver.com>
2024-06-01 02:32:18 +0900

    [Algo] add: n^2배열자르기 #20240601

---
+++ Programmers/pn^2배열자르기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class pn제곱배열자르기 {
+    public  List<Long> solution(int n, long left, long right) {
+        
+        List<Long> list = new ArrayList<>();
+        
+        for(long i=left;i<right+1;i++){
+             list.add(Math.max(i/n,i%n) + 1);
+        }
+        
+        return list;
+    }
+}
