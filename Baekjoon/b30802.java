cf63dad7c3402c50433a8173a2acad40154c7365
정민호 <allmin9702@naver.com>
2024-10-08 11:12:04 +0900

    [Algo] add: b30802,b31403 #20241008

---
+++ Baekjoon/b30802.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class b30802 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        Long N = Long.parseLong(br.readLine());
+        long[] arr = Arrays
+                .stream(br.readLine().split(" "))
+                .mapToLong(Long::parseLong)
+                .toArray();
+        long[] pf = Arrays.stream(br.readLine().split(" "))
+                .mapToLong(Long::parseLong)
+                .toArray();
+        long ans = 0;
+        for(int i = 0 ; i < arr.length ; i++) {
+            ans += arr[i] / pf[0];
+            ans = arr[i] % pf[0] == 0 ? ans : ans+1;
+        }
+        System.out.println(ans);
+        System.out.println(N/pf[1]+" "+N%pf[1]);
+    }
+}
