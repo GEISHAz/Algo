9408a146ccbf652903dbb23cf317da6c45b88472
정민호 <allmin9702@naver.com>
2024-11-29 18:24:42 +0900

    [Algo] add: b2143 #20241129

--- Baekjoon/b2143.java
+++ Baekjoon/b2143.java
@@ -1,7 +1,10 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main {
+public class b2143 {
 
     static int an, bn, t;
     static int[] A, B;
