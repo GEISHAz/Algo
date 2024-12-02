6f839f18869bf9ecda4a7fedf034f1b0c2b2a7b1
GEISHAz <allmin9702@naver.com>
2024-12-02 23:45:10 +0900

    [Algo] add: p거스름돈 #20241202

---
+++ Programmers/p거스름돈.java
@@ -0,0 +1,25 @@
+package Programmers;
+
+import java.util.Arrays;
+
+class p거스름돈 {
+
+	public int solution(int n, int[] money) {
+		Arrays.sort(money);
+
+		long[] dp = new long[n+1];
+
+		for(int i =0; i<=n ;i++){
+			if(i % money[0] == 0)
+				dp[i]=1;
+		}
+
+		for(int i=1; i<money.length ; i++){
+			for(int j=money[i]; j<=n; j++){
+				dp[j] += dp[j-money[i]];
+			}
+		}
+
+		return (int)(dp[n] % 1000000007);
+	}
+}
