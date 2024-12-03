1fc1175c68d7e6237a9d5c842f8a5f7332098e65
정민호 <allmin9702@naver.com>
2024-12-03 18:43:00 +0900

    [Algo] add: b2162 #20241203

--- Baekjoon/b2162.java
+++ Baekjoon/b2162.java
@@ -1,7 +1,9 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
+public class b2162 {
     static int N;
     static int[] parent, size;
 
