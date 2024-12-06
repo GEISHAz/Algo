fc1307bd590329915007d5f52317054cc6b18e10
정민호 <allmin9702@naver.com>
2024-12-06 15:04:09 +0900

    [Algo] add: b2166 #20241206

--- Baekjoon/b2166.java
+++ Baekjoon/b2166.java
@@ -1,7 +1,9 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
+public class b2166 {
 
     private static int N;
     private static long[][] arr;
