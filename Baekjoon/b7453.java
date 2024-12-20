5ccd10deb2d690d0c1a522f124d5e9c63381b4bd
정민호 <allmin9702@naver.com>
2024-12-20 16:02:17 +0900

    [Algo] add: b7453 #20241220

--- Baekjoon/b7453.java
+++ Baekjoon/b7453.java
@@ -1,7 +1,10 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main {
+public class b7453 {
     static int N;
 
     static int lowerBound(long target, long[] B) {
