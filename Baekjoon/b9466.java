f465e0680f12e2b4f7c188777b7bd7aa6d2dd5fe
정민호 <allmin9702@naver.com>
2024-12-31 19:48:24 +0900

    [Algo] add: b9466 #20241231

--- Baekjoon/b9466.java
+++ Baekjoon/b9466.java
@@ -1,9 +1,9 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class b9466 {
 
     static int n,count = 0;
     static int[] arr;
