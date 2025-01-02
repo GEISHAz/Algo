1af0f03714f6b0fe2f4d76ad61fea93a2c707ce0
GEISHAz <allmin9702@naver.com>
2025-01-02 23:53:27 +0900

    [Algo] add: b12100 #20250102

--- Baekjoon/Main.java
+++ Baekjoon/Main.java
@@ -1,54 +1,105 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int n,count = 0;
-    static int[] arr;
-    static boolean[] isVisited, isChecked;
+    static int N, answer = 0;
+    static int[][] board, saved;
 
-    static int stoi(String s){ return Integer.parseInt(s); }
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    static void merge() {
+        for (int i = 0; i < N; i++) {
+            int[] arr = new int[N];
+            int idx = 0;
+            for (int n : saved[i]) {
+                if (n != 0) arr[idx++] = n;
+            }
+            for (int j = 0; j < N - 1; j++) {
+                if (arr[j] == arr[j + 1]) {
+                    arr[j] *= 2;
+                    arr[j + 1] = 0;
+                    j++;
+                }
+            }
+            int[] ans = new int[N];
+            idx = 0;
+            for (int n : arr) {
+                if (n != 0) ans[idx++] = n;
+            }
+            saved[i] = ans;
+        }
+    }
+
+    static void rotate() {
+        int[][] rotated = new int[N][N];
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                rotated[N - j - 1][i] = saved[i][j];
+        saved = rotated;
+    }
+
+    static void flip() {
+        int[][] fliped = new int[N][N];
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                fliped[N - i - 1][N - j - 1] = saved[i][j];
+        saved = fliped;
+    }
+
+    static void applyDirection(int dir) {
+        if (dir == 1) {
+            merge();
+        } else if (dir == 2) {
+            flip();
+            merge();
+            flip();
+        } else if (dir == 3) {
+            rotate();
+            merge();
+        } else if (dir == 4) {
+            flip();
+            rotate();
+            merge();
+            rotate();
+        }
+    }
+
+    static void combination(int depth) {
+        if (depth == 5) {
+            for (int i = 0; i < N; i++)
+                for (int m : saved[i])
+                    answer = Math.max(answer, m);
+            return;
+        }
+        int[][] original = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            original[i] = saved[i].clone();
+        }
+        for (int dir = 1; dir <= 4; dir++) {
+            applyDirection(dir);
+            combination(depth + 1);
+            saved = original;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        int T = stoi(br.readLine());
-
-        for (int t = 0 ; t < T ; t++){
-            n = stoi(br.readLine());
-            arr = new int[n+1];
-            isVisited = new boolean[n+1];
-            isChecked = new boolean[n+1];
-            count = 0;
-
-            st = new StringTokenizer(br.readLine());
-            for (int i = 1; i <= n ; i++)
-                arr[i] = stoi(st.nextToken());
-
-            for (int i = 1; i <= n ; i++){
-                dfs(i);
-            }
-            System.out.println(n - count);
-        }
-    }
-    public static void dfs(int now){
-        if(isVisited[now]) return;
-
-        isVisited[now] = true;
-        int next = arr[now];
-
-        if (!isVisited[next])
-            dfs(next);
-        else {
-            if (!isChecked[next]){
-                count ++;
-                for (int i = next ; i != now ; i = arr[i])
-                    count++;
+        N = stoi(br.readLine());
+        board = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                board[i][j] = stoi(st.nextToken());
             }
         }
-        isChecked[now] = true;
+        saved = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            saved[i] = board[i].clone();
+        }
+        combination(0);
+        System.out.println(answer);
     }
 }
