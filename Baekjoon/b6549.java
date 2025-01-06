efe8eadeb4813c3b54ba6ea8e0b6f8be09bcbebc
정민호 <allmin9702@naver.com>
2025-01-06 15:33:36 +0900

    [Algo] add: b히스토그램에서 가장 큰 직사각형 #20250106

--- Baekjoon/b6549.java
+++ Baekjoon/b6549.java
@@ -1,8 +1,10 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main {
+public class b6549 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
