24abd55267f8edb34992926a4bfa8076ba7f3ccb
정민호 <allmin9702@naver.com>
2025-01-08 14:20:18 +0900

    [Algo] add: b1300 #20250108

--- Baekjoon/b1300.java
+++ Baekjoon/b1300.java
@@ -1,6 +1,8 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main {
+public class b1300 {
 
     static int N, M;
 
