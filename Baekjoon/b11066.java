3b171fb417ba0ab9a39edb29aa652e4ae926218a
정민호 <allmin9702@naver.com>
2025-01-10 13:57:42 +0900

    [Algo] add: b11066 #20250110

--- Baekjoon/b11066.java
+++ Baekjoon/b11066.java
@@ -1,6 +1,9 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-public class Main {
+
+public class b11066 {
 
     static int T, N;
     static int[][] dp;
