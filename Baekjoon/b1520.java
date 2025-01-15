589c104e53fb4b783fbe133df573b59c2c823772
정민호 <allmin9702@naver.com>
2025-01-15 13:58:35 +0900

    [Algo] add: b1520 #20250115

--- Baekjoon/b1520.java
+++ Baekjoon/b1520.java
@@ -1,8 +1,10 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class b1520 {
 
     static int N,M;
     static int[][] dir = {{0,1},{-1,0},{0,-1},{1,0}};
