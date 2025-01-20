07abc3259aae806a4c2d962b94c5d79d69d0ec01
정민호 <allmin9702@naver.com>
2025-01-20 13:16:29 +0900

    [Algo] add: p_thievery #20250120

---
+++ Programmers/p_thievery.java
@@ -0,0 +1,23 @@
+package Programmers;
+
+import java.util.*;
+
+class p_thievery {
+	int[] dp1,dp2;
+	public int solution(int[] money) {
+		int size = money.length;
+		dp1 = new int[size+1];
+		dp2 = new int[size+1];
+		dp1[0] = 0;
+		dp2[0] = 0;
+		for(int i = 0 ; i < size-1 ; i++){
+			dp1[i+1] = money[i];
+			dp2[i+1] = money[i+1];
+		}
+		for(int i = 2 ; i < dp1.length ; i++){
+			dp1[i] = Math.max(dp1[i-1],dp1[i-2]+dp1[i]);
+			dp2[i] = Math.max(dp2[i-1],dp2[i-2]+dp2[i]);
+		}
+		return Math.max(dp1[size-1],dp2[size-1]);
+	}
+}
