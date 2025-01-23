1a81128c2611c2aece0f09964ec0dc8d5001bb7f
정민호 <allmin9702@naver.com>
2025-01-23 15:54:05 +0900

    [Algo] add: b17298 #20250123

--- Baekjoon/b17298.java
+++ Baekjoon/b17298.java
@@ -1,9 +1,10 @@
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main {
+public class b17298 {
 
     static int N;
     static int[] numbers;
