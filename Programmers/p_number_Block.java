68000ab210bb86ea05e2b1add28b6b026db03ac5
GEISHAz <allmin9702@naver.com>
2025-02-03 15:01:14 +0900

    [Algo] add: pNumberBlock #20250203

---
+++ Programmers/p_number_Block.java
@@ -0,0 +1,27 @@
+package Programmers;
+
+class p_number_Block {
+	private int isPrime(int n){
+		if(n == 1) return 0;
+		int max = 1;
+
+		for(int i = 2 ; i <= Math.sqrt(n) ; i++){
+			if(n % i == 0){
+				if(n / i <= 10000000) return n / i;
+				max = i;
+			}
+		}
+		return max;
+	}
+
+	public int[] solution(long begin, long end) {
+		int[] result = new int[(int)(end - begin + 1)];
+		int index = 0;
+
+		for(long i = begin ; i <= end ; i++){
+			result[index++] = isPrime((int)i);
+		}
+
+		return result;
+	}
+}
