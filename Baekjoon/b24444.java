70daf9a063cecf948fd1e9a2949be1d61796a265
정민호 <allmin9702@naver.com>
2025-03-04 15:40:26 +0900

    [Algo] add: b24444 #20250304

--- Baekjoon/b24444.java
+++ Baekjoon/b24444.java
@@ -1,7 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
-public class Main {
+public class b24444 {
 
     static int N, M, R;
     static ArrayList<Integer>[] list;
