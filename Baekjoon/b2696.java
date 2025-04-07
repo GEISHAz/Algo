df7c4ebdd171b26465ac1fd08380ac284657bd82
정민호 <allmin9702@naver.com>
2025-04-07 14:14:26 +0900

    [Algo] add: b2696 #20250407

--- Baekjoon/b2696.java
+++ Baekjoon/b2696.java
@@ -1,7 +1,12 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Main {
+public class b2696 {
 
     static int T;
 
