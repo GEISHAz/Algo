bdce1e80bce2c2c95823243038df70125102b18f
정민호 <allmin9702@naver.com>
2025-04-09 16:43:56 +0900

    [Algo] add: b3665 #20250409

--- Baekjoon/b3665.java
+++ Baekjoon/b3665.java
@@ -1,7 +1,12 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
-public class Main {
+public class b3665 {
 
     static int T;
 
