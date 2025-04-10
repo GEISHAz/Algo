b7a77bde5185c2b0fba23a7c19f28394cea8dec7
정민호 <allmin9702@naver.com>
2025-04-10 14:27:18 +0900

    [Algo] add: b1450 #20250410

--- Baekjoon/b1450.java
+++ Baekjoon/b1450.java
@@ -1,10 +1,12 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
-public class Main {
+public class b1450 {
 
     static int N, C;
     static int[] weights;
