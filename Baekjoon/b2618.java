a742cb8772d1c9ee7ac58eefa1ee6e11e691bc15
정민호 <allmin9702@naver.com>
2025-04-18 20:14:38 +0900

    [Algo] add: b_Police_car #20250418

--- Baekjoon/b2618.java
+++ Baekjoon/b2618.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class b2618 {
 
     public static int[][] list = new int[1002][2];
     public static int event_num, line_num;
