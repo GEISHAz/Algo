3b8a99d165aa09a2ba72c1e746e32305e2ef4ec9
정민호 <allmin9702@naver.com>
2025-04-22 21:18:19 +0900

    [Algo] add: b_Hide_And_Seek #20250422

--- Baekjoon/b13913.java
+++ Baekjoon/b13913.java
@@ -1,9 +1,9 @@
-    import java.io.BufferedReader;
-    import java.io.IOException;
-    import java.io.InputStreamReader;
-    import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
-    public class Main {
+    public class b13913 {
 
         static int[] dp,prav;
         static int start, end;
