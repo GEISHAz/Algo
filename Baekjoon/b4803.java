887b65ff9a4d80963cd61cb4eceee8267f41795e
정민호 <allmin9702@naver.com>
2025-04-28 16:10:12 +0900

    [Algo] add: b4803_tree #20250428

--- Baekjoon/b4803.java
+++ Baekjoon/b4803.java
@@ -1,11 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
-public class Main {
+public class b4803 {
 
     static int N,M;
     static int[] p;
