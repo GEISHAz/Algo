df302f46c32f9050e790ceb0e88107bacb5c43a1
정민호 <allmin9702@naver.com>
2025-05-06 19:37:58 +0900

    [Algo] add: b1717_expression_of_set #20250506

--- Baekjoon/b1717.java
+++ Baekjoon/b1717.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class b1717 {
 
     static int n,m;
     static int[] p;
