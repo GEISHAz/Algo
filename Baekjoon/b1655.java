4bf39df29ca5d7512f96740134fd4901d57b06d8
정민호 <allmin9702@naver.com>
2025-05-19 14:21:38 +0900

    [Algo] add: b1655_Say_the_middle #20250519

--- Baekjoon/b1655.java
+++ Baekjoon/b1655.java
@@ -3,7 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.PriorityQueue;
-public class Main {
+
+public class b1655 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
