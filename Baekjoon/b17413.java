823015dc0beeb963fdb0b031d7c71f2f25e4f3e5
정민호 <allmin9702@naver.com>
2025-05-31 21:22:08 +0900

    [Algo] add: b17413_Chonsu_calculate #20250531

--- Baekjoon/b17413.java
+++ Baekjoon/b17413.java
@@ -1,6 +1,8 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main {
+public class b17413 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
