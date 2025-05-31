af613fb6854c0bf2fb69c7dc554c5acd7703bf5d
정민호 <allmin9702@naver.com>
2025-05-31 21:29:41 +0900

    [Algo] add: b1002_Turret #20250531

--- Baekjoon/b1002.java
+++ Baekjoon/b1002.java
@@ -1,7 +1,7 @@
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.util.Scanner;
 
-public class Main {
+public class b1002 {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
 
