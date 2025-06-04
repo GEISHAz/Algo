1010fbf42828e4c0475ac57ec83be99d8636d909
정민호 <allmin9702@naver.com>
2025-06-04 18:29:44 +0900

    [Algo] add: b11062_Card_game #20250604

--- Baekjoon/b11062.java
+++ Baekjoon/b11062.java
@@ -1,7 +1,9 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
-public class Main {
+public class b11062 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
