98a17d8bbf10471dfb89866230ad322d33730943
정민호 <allmin9702@naver.com>
2025-06-04 21:29:40 +0900

    [Algo] add: b2193_leechinsoo #20250604

--- Baekjoon/b2193.java
+++ Baekjoon/b2193.java
@@ -1,6 +1,8 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main {
+public class b2193 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
