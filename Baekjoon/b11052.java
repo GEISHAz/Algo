220acb1c797166c1ebfc9d287b1ef75b4ce2bc10
정민호 <allmin9702@naver.com>
2025-06-05 16:37:12 +0900

    [Algo] add: b11052_buy_the_card #20250605

--- Baekjoon/b11052.java
+++ Baekjoon/b11052.java
@@ -1,7 +1,9 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Main {
+public class b11052 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
