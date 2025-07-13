f4892d4d4036a49bdba1776d88b2b11f74cb6302
정민호 <allmin9702@naver.com>
2025-07-13 20:27:58 +0900

    [Algo] add: b1106_hotel #20250713

--- Baekjoon/b1106.java
+++ Baekjoon/b1106.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class b1106 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
